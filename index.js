37ef55e19229c6b74d9b752e6a6b006d9143aa99
Railson alves <raialvess3991@gmail.com>
2023-12-01 23:29:13 -0300

    ultimo comentario

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let ranking = "FERRO"
 const menssagem1 = "O heroi de nome "
 const menssagem2 = " esta no nivel de "
 
+
 if(xpHeroi <= 1000){
     console.log(menssagem1 + nomeHeroi + menssagem2 + ranking)
 }
